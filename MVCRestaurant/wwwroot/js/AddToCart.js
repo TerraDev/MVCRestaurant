10e22d3ce9b2850aeb1189ab9e88e114a1fec275
B A <43823048+TerraDev@users.noreply.github.com>
2023-09-14 11:42:04 +0330

    Add files via upload

---
+++ MVCRestaurant/wwwroot/js/AddToCart.js
@@ -0,0 +1,119 @@
+﻿function AddToCart(itemid, inc) {
+    //console.log(event.target)
+    var tar = event.target
+    $.ajax({
+        url: "/Order/AddToCart?itemId=" + itemid + "&increment=" + inc,
+        type: 'GET',
+        dataType: 'json', //data type
+        success: function (res) {
+            //console.log("++Message: \n");
+            //console.log(res)
+            //console.log(res.quantity)
+            if (res.quantity > 0) {
+                createQuantityControllerUI(itemid, res.quantity, tar)
+            }
+            else {
+                createOrderButton(itemid, tar);
+            }
+            setShoppingCounter(res.total)
+        },
+        error: function (jqXHR, exception) {
+            console.log(jqXHR);
+            // error handling logic here...
+        }
+    });
+}
+
+
+//function AddToCart_t(itemid, inc) {
+//    console.log(event.target);
+//    max = 10; min = 1;
+//    var r = Math.floor(Math.random() * (max - min + 1) + min)
+//    console.log("random number is: " + r)
+//    if (r > 2) {
+//        createQuantityControllerUI(itemid, r)
+//    }
+//    else {
+//        createOrderButton(itemid);
+//    }
+//}
+
+//QC-UI
+function createQuantityControllerUI(itemId, quantity, tar) {
+    //debugger;
+    //console.log(event.target);
+    QCdiv = document.createElement('template');
+    QCdiv.innerHTML =
+        '<span id=foodItem_' + itemId + ' class="QCUI">' +
+        '<i class="bi bi-caret-up-square-fill" style="display:inline;" onclick="AddToCart(' + itemId + ', true)"></i> ' +
+        '<p style="display:inline"> ' + quantity + ' </p>' +
+        '<i class="bi bi-caret-down-square-fill" style="display:inline" onclick="AddToCart(' + itemId + ', false)"></i>' +
+        '</span >';
+    if (tar.tagName != "I") {
+        tar.replaceWith(QCdiv.content.firstChild)
+    }
+    else {
+        tar.parentNode.replaceWith(QCdiv.content.firstChild)
+    }
+    
+}
+
+function createOrderButton(itemId, tar) {
+    QCdiv = document.createElement('template');
+    QCdiv.innerHTML =
+    '<a class="btn btn-primary" onclick="AddToCart(' + itemId + ',true)">افزودن به سبد خرید</a>'
+
+    if (tar.tagName != "I") {
+        tar.replaceWith(QCdiv.content.firstChild)
+    }
+    else {
+        tar.parentNode.replaceWith(QCdiv.content.firstChild)
+    }
+}
+
+function getOrdersCount(func) {
+    $.ajax({
+        url: "/Order/TotalCount",
+        type: 'GET',
+        dataType: 'json', //data type
+        success: function (res) {
+            cnt = res.quantity
+            //console.log("1: " + cnt);
+            func(cnt);
+        },
+        error: function (jqXHR, exception) {
+            console.log(jqXHR);
+            // error handling logic here...
+            func(0);
+        }
+    });
+
+}
+
+function setShoppingCounter(cnt) {
+    let div = document.getElementById("ItemsQuantity");
+    let div2 = document.getElementById("navOrderButton");
+    if (cnt > 0) {
+        div.innerHTML = cnt
+        div2.style.display = "block";
+    }
+    else {
+        div.innerHTML = ""
+        div2.style.display = "none";
+    }
+    //console.log("3: " + cnt);
+}
+
+//window.addEventListener('pageshow', function () {
+//    console.log("window.onload1");
+//    getOrdersCount(setShoppingCounter);
+//    console.log("window.onload2");
+//    alert('window.addEventListener');
+//})
+
+
+$(document).ready(function () {
+    //console.log("window.onload1");
+    getOrdersCount(setShoppingCounter);
+    //console.log("window.onload2");
+})
---
+++ MVCRestaurant/wwwroot/js/site.js
@@ -0,0 +1,59 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+function DeclareModal(containerId, ModalId, ModalBodyId, buttonId, TitleId, CloseModalId, ActionModalId) {
+    $("#" + containerId)
+        .html(
+            `<div class=modal fade" id="${ModalId}" tabindex=" - 1" role="dialog" aria-labelledby="${ModalId}" aria-hidden="true">
+        <div class= "modal-dialog modal-lg modal-dialog-centered" role="document" >
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 id="${TitleId}" class="modal-title" style="margin: 0 auto;" id="ModalLongTitle"></h5>
+            </div>
+            <div id="${ModalBodyId}" class="p-3">
+            </div>
+            <div class="modal-footer ml-auto">
+          <button id="${CloseModalId}" type="button" class="btn btn-secondary mr-2" data-bs-dismiss="modal">بستن</button>
+          <button id="${ActionModalId}" type="button" class="btn btn-primary mr-2" onclick="$('#${buttonId}').click()">ذخیره تغییرات</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<style>
+    #${ModalBodyId} form .form-group{
+        margin-top:6px;
+        margin-bottom:6px
+    }
+</style>`)
+
+    function EnhanceAjaxForm() {
+        this.setAttribute("data-ajax", "true")
+        this.setAttribute("data-ajax-dataType", "html")
+        this.setAttribute("data-ajax-update", "#" + ModalBodyId)
+        this.setAttribute("data-ajax-success", "DeclareModal.EnhanceAjaxForm.DataSuccess")
+        this.setAttribute("data-ajax-failure", "DeclareModal.EnhanceAjaxForm.DataFailure")
+
+        function DataSuccess(response) {
+            //alert("success!");
+            $("#" + ModalBodyId).html('<p dir="ltr" class="text-start">Finishing...</p>');
+            $("#" + ModalId + " button").hide();
+            ToastrSuccess()
+            setTimeout(function () {
+                $("#" + ModalId).modal('hide');
+                location.replace(location.href);
+            }, 2200)
+        }
+        EnhanceAjaxForm.DataSuccess = DataSuccess
+
+        function DataFail(response) {
+            ToastrFail()
+            $("#" + ModalBodyId).html(response.responseText);
+        }
+        EnhanceAjaxForm.DataFailure = DataFail
+    }
+
+    DeclareModal.EnhanceAjaxForm = EnhanceAjaxForm;
+}
+
